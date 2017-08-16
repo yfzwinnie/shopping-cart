014c2d0b589f394ee0532b45b921ea405a62302c
Winnie Zhang <yfz.winnie@gmail.com>
2017-08-16 13:22:18 -0700

    added buy now button

--- cart.js
+++ cart.js
@@ -1,14 +1,3 @@
-// var products = $(".col-md-6");
-// var orderItems = [];
-// products.each(function(i, product) {
-//   var orderItem = {};
-//   orderItem["name"] = $(product).children("p").html();
-//   orderItem["price"] = $(product).children("p").next().html();
-//   console.log(orderItem);
-//   orderItems.push(orderItem);
-// });
-// console.log(orderItems);
-
 const products = $(".col-md-6");
 products.each(function(i, product) {
   $(product).append('<input type="button" value="Add to Cart" class="btn-success">');
@@ -17,20 +6,20 @@ products.each(function(i, product) {
 const cart = $('.cart');
 const orderItems = [];
 $(".btn-success").click(function () {
+    // Adding name and price of selected item into orderItem array
     const orderItem = {};
     orderItem["name"] = $(this).prev().prev().html();
     orderItem["price"] = $(this).prev().html();
     orderItems.push(orderItem);
-
+    // adding item to cart
     const itemsOrdered = $('<div class="items-ordered"></div>');
     var subtotal = 0;
     var tax = 0;
     var total = 0;
     $(orderItems).each(function(i, item) {
-      // adding item to cart
       itemsOrdered.html(item.name + ' ' + item.price);
       $('.cart').append(itemsOrdered);
-      // converting price to float and adding to subtotal
+      // converting price to float and calculating subtotal, tax, total
       price = item.price.substring(1);
       subtotal += parseFloat(price)
       $('#subtotal').html("Subtotal: $" + subtotal);
@@ -38,7 +27,7 @@ $(".btn-success").click(function () {
       $('#tax').html("Tax: $" + tax.toFixed(2));
       total = subtotal + tax;
       $('#total').html("Total: $" + total.toFixed(2));
+      // make the buy now button visible
+      $('#buy-now').css('visibility', 'visible');
     });
-
-
 });
